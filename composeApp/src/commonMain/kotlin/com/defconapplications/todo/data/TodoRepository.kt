8cd8224f97c5decc11348c5d79e3934ba7ecf9b7
BDemutAllegro <bartosz.demut@allegro.pl>
2024-06-03 12:23:48 +0200

    ui stuff

--- composeApp/src/commonMain/kotlin/com/defconapplications/todo/data/TodoRepository.kt
+++ composeApp/src/commonMain/kotlin/com/defconapplications/todo/data/TodoRepository.kt
@@ -9,6 +9,11 @@ class TodoRepository {
     val items = listOf(
         TodoItem("Task 1", Deadline.Date(LocalDate(2024, Month.DECEMBER, 1))),
         TodoItem("Task 2", Deadline.DateTime(LocalDate(2024, Month.MAY, 6), LocalTime(7, 30))),
-        TodoItem("Task 3", Deadline.Date(LocalDate(2024, Month.JUNE, 15)))
+        TodoItem("Task 3", Deadline.Date(LocalDate(2024, Month.JUNE, 15))),
+        TodoItem("Task 4", Deadline.Date(LocalDate(2024, Month.JUNE, 16))),
+        TodoItem("Task 5", Deadline.Date(LocalDate(2024, Month.JUNE, 16))),
+        TodoItem("Task 6", Deadline.Date(LocalDate(2024, Month.JUNE, 1))),
+        TodoItem("Task 7", Deadline.Date(LocalDate(2024, Month.JUNE, 2))),
+        TodoItem("Task 8", Deadline.Date(LocalDate(2024, Month.JUNE, 3)))
     )
 }
--- composeApp/src/commonMain/kotlin/com/defconapplications/todo/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/defconapplications/todo/ui/home/HomeScreen.kt
@@ -2,6 +2,8 @@ package com.defconapplications.todo.ui.home
 
 import ViewTodoItem
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -10,13 +12,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.defconapplications.todo.ui.theme.TodoTheme
@@ -29,26 +43,61 @@ fun HomeScreen(
     viewModel: HomeScreenViewModel = koinViewModel()
 ) {
     TodoTheme {
-        val state = viewModel.state.collectAsState().value
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Scaffold(
+            topBar = { TopBar() },
+            floatingActionButton = { AddItemButton() }
         ) {
-            state.sections.forEach { section ->
-                item { SectionHeader(section.title) }
-                items(section.items) { item ->
-                    TodoItem(item)
+            val state = viewModel.state.collectAsState().value
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(it),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                state.sections.forEach { section ->
+                    item {
+                        Card(
+                            modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+                        ) {
+                            Column {
+                                SectionHeader(section.title)
+                                for (item in section.items) {
+                                    TodoItem(item)
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(title = {
+        Text("To do")
+    })
+}
+
+@Composable
+fun AddItemButton() {
+    FloatingActionButton(
+        onClick = {}
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "add an item"
+        )
+    }
+}
+
 @Composable
 fun SectionHeader(text: String) {
     Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
+        modifier = Modifier.fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp),
         text = text
     )
 }
@@ -56,13 +105,19 @@ fun SectionHeader(text: String) {
 @Composable
 fun TodoItem(item: ViewTodoItem) {
     Row(
-        modifier = Modifier.padding(8.dp)
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
+        Checkbox(
+            checked = false, onCheckedChange = {}
+        )
         Text(
             modifier = Modifier.weight(1f),
             text = item.title
         )
-        VerticalDivider()
-        Text(item.deadline)
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = item.deadline
+        )
     }
 }
